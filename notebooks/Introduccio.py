aecba106cd025dccbadcaf69ccbe7f121a4707d9
Gabriel Jover-Manas <gjover@cells.es>
2023-02-26 18:43:15 +0100

    Replace Introduccio with jupytext version

---
+++ notebooks/Introduccio.py
@@ -0,0 +1,115 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# # Modelització
+# ## Introducció
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# ## Què és un model?
+
+# %% [markdown] slideshow={"slide_type": "fragment"}
+# * Eina per resoldre problemes amb precisió
+
+# %% [markdown] slideshow={"slide_type": "fragment"}
+# * Simplificat i eficient
+
+# %% [markdown] slideshow={"slide_type": "fragment"}
+# * Validat vers la realitat
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# ## El Cicle de modelització
+#
+# <center></br>
+# <img src=./static/cicle_modelitzacio.png width=75%>
+# </center>
+#
+
+# %% [markdown] slideshow={"slide_type": "notes"}
+# - És una guia de bones pràctiques
+# - No és un cicle rígid
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# ## Formulació del problema
+# - Àmbit d'estudi
+# - Objectiu que es busca
+#     - Precisió de l'estudi
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# ## Anàlisis del sistema
+# - Context
+# - Factors i esdeveniments mesurables
+# - Relacions, comportament i estats
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# ## Modelat
+# - Representació matemàtica de variables i relacions
+# - Anàlisi del model
+# - Interpretació del model en el context
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# ## Recol·lecció i anàlisis de dades
+# - Experiment
+# - Procés de mesura
+# - Ajust del model a les dades
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# ## Validació i verificació del model
+# - El model serveix per resoldre el problema
+# - El model és prou precís
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# ## Interpretació dels resultats
+# - Anàlisi crítica
+# - Revisió i millora del model
+#     - [Tancant el cicle](#/slide-2-0)
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+#  
+
+# %% [markdown] slideshow={"slide_type": "skip"}
+# # Bibliografia
+#
+# - <a id='bib_Edwards'></a> Edwards, D., **2001**. *Guide to mathematical modelling*. Palgrave, Basingstoke.
+
+# %% [markdown] slideshow={"slide_type": "skip"}
+# Copyright (C) 2022 Gabriel Jover Mañas
+#
+#     Permission is granted to copy, distribute and/or modify this document
+#     under the terms of the GNU Free Documentation License, Version 1.3
+#     or any later version published by the Free Software Foundation;
+#     with no Invariant Sections, no Front-Cover Texts, and no Back-Cover Texts.
+#     A copy of the license is included in the section entitled "GNU
+#     Free Documentation License".
+#
+# ![GFDL][GFDL]
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# ![GPL3][GPL3]
+#
+# [GFDL]: https://www.gnu.org/graphics/gfdl-logo.svg
+# [GPL3]: https://www.gnu.org/graphics/gplv3-or-later.svg
